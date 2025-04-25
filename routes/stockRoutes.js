11b6dd3a0fcf4dd65915cff4581b5dd710c5dbf7
kaybe005 <kalashbijukchhe74@gmail.com>
2025-04-26 08:03:31 +1000

    Clean up old backend files after move

--- routes/stockRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getStockInfo } from '../controllers/stockController.js';
-
-const router = express.Router();
-
-router.get('/stocks/:symbol', getStockInfo);
-
-export default router;
