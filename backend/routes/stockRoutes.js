54c5f23bfc2c087b32aa533a21115fa03ecb4331
kaybe005 <kalashbijukchhe74@gmail.com>
2025-04-29 15:59:09 +1000

    Add chart data route and controller for dashboard

--- backend/routes/stockRoutes.js
+++ backend/routes/stockRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getStockInfo } from '../controllers/stockController.js';
+import { getStockInfo, getChartData } from '../controllers/stockController.js';
+
 
 const router = express.Router();
 
 router.get('/stocks/:symbol', getStockInfo);
+router.get('/chart', getChartData);
 
 export default router;
