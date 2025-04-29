190349f0a5bedc98005db3f42cb3dadefc84e998
kaybe005 <kalashbijukchhe74@gmail.com>
2025-04-29 18:39:19 +1000

    Fixing annoying error

--- backend/routes/stockRoutes.js
+++ backend/routes/stockRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { getStockInfo, getChartData } from '../controllers/stockController.js';
 
-
 const router = express.Router();
 
 router.get('/stocks/:symbol', getStockInfo);
+
 router.get('/chart', getChartData);
 
 export default router;
